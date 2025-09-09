8677c2c1b3a8d53f6a28ec37ab48525c95e438ac
jengahmedeyan <58713713+jengahmedeyan@users.noreply.github.com>
2025-09-09 13:46:53 +0000

    Implement code changes to enhance functionality and improve performance

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   async headers() {
+  // Set the output file tracing root to the current project directory
+  outputFileTracingRoot: __dirname,
+  
+  async headers() {
     return [
       {
         source: '/sw.js',
